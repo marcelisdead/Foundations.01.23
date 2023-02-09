1897f33b5d215dfd45bbfcac2d75cd97f420ac2c
marcelisdead <46794418+marcelisdead@users.noreply.github.com>
2023-02-08 22:57:53 -0500

    more simple ScaleForever Script

--- Assets/_Assets/Scripts/ScaleForever.cs
+++ Assets/_Assets/Scripts/ScaleForever.cs
@@ -5,21 +5,20 @@ using UnityEngine;
 /// <summary>
 /// This script scales its transform up over time.
 /// 
-/// We will make this script to learn about variables and functions and Vector3 math.
+/// We will make this script to learn about variables and functions
+/// and Vector3 math.
 /// 
 /// Tip: To make your object grow like a tree,
-/// place this script on a parent positioned where your object contacts the ground.
+/// place this script on a parent positioned where your object 
+/// contacts the ground.
 /// </summary>
 
 public class ScaleForever : MonoBehaviour
 {
-    public Vector3 setScale = new Vector3(1, 1, 1);
-    public float multiplier = 0.1f;
-
+    public Vector3 setScale = new Vector3(1.0f, 1.0f, 1.0f);
     // Start is called before the first frame update
     void Start()
     {
-        //setScale = new Vector3(1.5f, 1.5f, 1.5f);
         //transform.localScale = transform.localScale + setScale;
     }
 
@@ -29,10 +28,11 @@ public class ScaleForever : MonoBehaviour
         transform.localScale = ScaleVector(transform.localScale);
     }
 
+
     Vector3 ScaleVector(Vector3 inVector)
     {
         Vector3 outVector;
-        outVector = inVector + setScale * multiplier * Time.deltaTime;
+        outVector = inVector + setScale * Time.deltaTime;
         return outVector;
     }
 }
